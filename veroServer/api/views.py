5cdddbc6b62d833550f4a6efc2cf5559feac25d6
Enes Başkale <enesbaskale@gmail.com>
2023-10-19 16:12:58 +0300

    Initial commit

---
+++ veroServer/api/views.py
@@ -0,0 +1,17 @@
+from rest_framework.response import Response
+from rest_framework.decorators import  api_view
+import requests
+
+url = "https://api.baubuddy.de/index.php/login"
+payload = {
+    "username": "365",
+    "password": "1"
+}
+headers = {
+    "Authorization": "Basic QVBJX0V4cGxvcmVyOjEyMzQ1NmlzQUxhbWVQYXNz",
+    "Content-Type": "application/json"
+}
+
+response = requests.post(url, json=payload, headers=headers)
+data = response.json()
+access_token = data.get("oauth", {}).get("access_token")
