d78f4308e6a11142b6ad29ecc7e971aecd9f6a9d
Enes Başkale <enesbaskale@gmail.com>
2023-10-20 12:36:25 +0300

    filtered json file
    Not csv! json file is filtered("hu": null)

--- veroServer/api/views.py
+++ veroServer/api/views.py
@@ -1,6 +1,8 @@
 from rest_framework.response import Response
 from rest_framework.decorators import  api_view
 import requests
+import json
+import pandas as pd
 
 url = "https://api.baubuddy.de/index.php/login"
 payload = {
@@ -23,9 +25,18 @@ headers={
 }
 response = requests.get(url, headers=headers)
 if response.status_code ==200:
-    csv_data = response.text
-    with open("output.csv", "w") as f:
 
-        f.write(csv_data)
+    json_data = response.json()
+    with open("output.json", "w") as json_file:
+        json.dump(json_data, json_file)
+
+    with open('output.json', 'r', encoding="utf-8") as json_for_filter:
+        data = json.load(json_for_filter)
+
+    filtered_data = [item for item in data if item['hu'] is not None]
+
+    with open('filtered_output.json', 'w', encoding="utf-8") as filtered_json_file:
+        json.dump(filtered_data, filtered_json_file, indent=4, ensure_ascii=False)
+
 else:
     print("ERROR:", response.status_code)
