4ec5c07042674da9b11013397627081b3a548044
Enes Başkale <enesbaskale@gmail.com>
2023-10-25 17:12:46 +0300

    NaN "", "nan" problem
    solved

--- veroServer/api/views.py
+++ veroServer/api/views.py
@@ -3,26 +3,30 @@ from rest_framework.decorators import api_view
 import requests
 import json
 import pandas as pd
+import numpy as np
 
-url = "https://api.baubuddy.de/index.php/login"
-payload = {
-    "username": "365",
-    "password": "1"
-}
-headers = {
-    "Authorization": "Basic QVBJX0V4cGxvcmVyOjEyMzQ1NmlzQUxhbWVQYXNz",
-    "Content-Type": "application/json"
-}
+def get_access_token():
 
-response = requests.post(url, json=payload, headers=headers)
-data = response.json()
-access_token = data.get("oauth", {}).get("access_token")
+    url = "https://api.baubuddy.de/index.php/login"
+    payload = {
+        "username": "365",
+        "password": "1"
+    }
+    headers = {
+        "Authorization": "Basic QVBJX0V4cGxvcmVyOjEyMzQ1NmlzQUxhbWVQYXNz",
+        "Content-Type": "application/json"
+    }
+    response = requests.post(url, json=payload, headers=headers)
+    data = response.json()
+    access_token = data.get("oauth", {}).get("access_token")
+    return access_token
+
 
 url = "https://api.baubuddy.de/dev/index.php/v1/vehicles/select/active"
 headers = {
-    "Authorization": f"Bearer {access_token}"
+    "Authorization": f"Bearer {get_access_token()}"
 }
-print(access_token)
+print(get_access_token())
 response = requests.get(url, headers=headers)
 if response.status_code == 200:
 
@@ -31,7 +35,10 @@ if response.status_code == 200:
     csv_data = pd.read_csv('vehicles.csv', sep=';')
     json_data_df = pd.DataFrame(json_data)
 
+
     merged_df = csv_data.merge(json_data_df, on='kurzname', how='outer')
+
+
 
     merged_df['labelIds_x'] = merged_df['labelIds_x'].astype(str)
     merged_df['labelIds_y'] = merged_df['labelIds_y'].astype(str)
@@ -43,12 +50,51 @@ if response.status_code == 200:
     merged_df['lagerort_x'].fillna(merged_df['lagerort_y'], inplace=True)
     merged_df['labelIds_x'].fillna(merged_df['labelIds_y'], inplace=True)
 
+
+
+
+
+
     merged_df = merged_df.drop(['gruppe_y', 'langtext_y', 'info_y', 'lagerort_y', 'labelIds_y'], axis=1)
     merged_df.columns = merged_df.columns.str.rstrip('_x')
 
+
     hu_filtered_df = merged_df[merged_df['hu'].notna()]
 
-    hu_filtered_df = hu_filtered_df.where(pd.notna(hu_filtered_df), None)
+    hu_filtered_df = hu_filtered_df.replace("NaN", np.nan)
+
+    hu_filtered_df['profilePictureUrl'] = hu_filtered_df['profilePictureUrl'].apply(lambda x: None if pd.isna(x) else x)
+    hu_filtered_df['thumbPathUrl'] = hu_filtered_df['thumbPathUrl'].apply(lambda x: None if pd.isna(x) else x)
+
+
+    for column in hu_filtered_df.columns:
+        hu_filtered_df.loc[hu_filtered_df[column].isin(["nan", "", "NaN"]), column] = None
+
+
+
+    for index, row in hu_filtered_df.iterrows():
+        label_id = row['labelIds']
+
+        #if label_id != "nan":
+        if label_id == "sdkgg":
+            url= f'https://api.baubuddy.de/dev/index.php/v1/labels/{label_id}'
+            headers = {
+                "Authorization": f"Bearer {get_access_token()}"
+            }
+            response = requests.get(url, headers=headers)
+
+            if response.status_code ==200:
+                label_data = response.json()
+                color_code = label_data[0]['colorCode']
+                hu_filtered_df.at[index, 'colorCode'] = str(color_code)
+
+            else:
+                print(f'Error: There is no true formatted colorCode: {label_id}')
+
+
+
+
+
     hu_filtered_df = hu_filtered_df.to_dict(orient='records')
     with open('filtered_data.json', 'w', encoding="utf-8") as merged_json_file:
         json.dump(hu_filtered_df, merged_json_file, ensure_ascii=False, indent=4)
