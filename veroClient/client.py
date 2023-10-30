3d4a6ebfc2d8642d3d0e7df4a9550ce7cc89189f
Enes Başkale <93766125+ExNot@users.noreply.github.com>
2023-10-30 17:01:49 +0300

    command lines
    are done

--- veroClient/client.py
+++ veroClient/client.py
@@ -1,78 +1,67 @@
 import requests
 import json
 import pandas as pd
-import numpy as np
 from datetime import datetime
-import openpyxl as px
 from openpyxl.styles import PatternFill, Font
 from openpyxl.workbook import Workbook
 import datetime as dt
 import argparse
 
-
+# Parse command-line arguments
 parser = argparse.ArgumentParser(description='Process some values')
 parser.add_argument('-k', '--keys', nargs='+', help='additional keys')
 parser.add_argument('-c', '--colored', action='store_true', help='flag for coloring')
 args = parser.parse_args()
 
-
-
+# Get today's date in ISO format
 _today = dt.date.today()
 today_iso = _today.strftime("%Y-%m-%d")
 
-
-
+# Read data from 'vehicles.csv' file
 with open('vehicles.csv', 'r', encoding='utf-8') as file:
     csv_data = file.read()
 
+# Send a POST request to the API
 url = 'http://127.0.0.1:8000/api/upload-vehicles/'
-
 response = requests.post(url, data={'csv_data': csv_data})
 data = response.json()
 
-if response.status_code == 200:
-    print("success")
-else:
-    print("CSV dosyasını sunucuya gönderirken bir hata oluştu. HTTP Kodu:", response.status_code)
-
-
-
-
-
-colored = True
-
 today = datetime.now()
+
+# Function to calculate color code for 'hu' column
 def calculate_color_code_hu(hu):
-
-
     if hu is None:
         return None
 
     hu_date = datetime.strptime(hu, "%Y-%m-%d")
-
     if hu_date > today:
         return None
 
-    date_gap = today-hu_date
-
-    if date_gap.days <90:
-        return "#007500"
-    elif 90<= date_gap.days <365:
-        return "#FFA500"
+    date_gap = today - hu_date
+    if date_gap.days < 90:
+        return "#007500"  # Green
+    elif 90 <= date_gap.days < 365:
+        return "#FFA500"  # Orange
     else:
-        return "#b30000"
-
-
+        return "#b30000"  # Red
 
 json_data = data['data']
-
 
 data_dict = json.loads(json_data)
 
 df = pd.DataFrame(data_dict)
 
+# Sort data by 'gruppe' column
 data = df.sort_values(by='gruppe')
 
+if args.keys:
+    valid_args = data.columns
+    for arg in args.keys:
+        if arg not in valid_args:
+            print(f"Invalid argument: {arg}")
+            exit(1)
+
+# Select columns specified by arguments
 selected_columns = ['rnr'] + [key for key in args.keys if key != 'colorCode' and key != 'labelIds']
 data_selected = data[selected_columns]
 
@@ -81,6 +70,7 @@ color_selected = data[color_selected]
 
 header = data_selected.columns.tolist()
 
+# Create a new Excel workbook
 wb = Workbook()
 ws = wb.active
 ws.append(list(data_selected.columns))
@@ -89,11 +79,14 @@ fill_green = PatternFill(start_color="007500", end_color="007500", fill_type="so
 fill_orange = PatternFill(start_color="FFA500", end_color="FFA500", fill_type="solid")
 fill_red = PatternFill(start_color="b30000", end_color="b30000", fill_type="solid")
 
+# Iterate through the data and add rows to the worksheet
 for _, row in data.iterrows():
     row_data = [row[column] for column in data_selected.columns]
     ws.append(row_data)
 
+    # Calculate color code for cells based on 'hu' column
     color_code_for_cells = calculate_color_code_hu(row['hu'])
+
     if args.colored:
         if color_code_for_cells == "#007500":
             for row in ws.iter_rows(min_row=ws.max_row, max_row=ws.max_row, min_col=1,
@@ -111,38 +104,17 @@ for _, row in data.iterrows():
                 for cell in row:
                     cell.fill = fill_red
 
+# Apply font color to cells in 'labelIds' column
 if 'labelIds' in args.keys:
     color_cod_col = color_selected['colorCode']
 
     for idx in range(len(color_cod_col)):
         color_code = color_cod_col.iloc[idx]
         if not pd.isna(color_code):
-            row = ws[idx+2]
+            row = ws[idx + 2]  # Adjust the row index
             for cell in row:
                 cell.font = Font(color=color_code)
 
-
-
+# Save the Excel file with a filename based on today's date
 filename = f'vehicles_{today_iso}.xlsx'
 wb.save(filename)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- veroServer/api/views.py
+++ veroServer/api/views.py
@@ -1,21 +1,16 @@
+# Import necessary modules
 import requests
 import json
 import pandas as pd
-import csv
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from io import StringIO
-<<<<<<< HEAD
-=======
 import warnings
-from django.http import HttpResponse
 
+# Optionally disable warnings
+#warnings.filterwarnings("ignore") # Use this to ignore the warning: A value is trying to be set on a copy of a slice from a DataFrame. Try using .loc[row_indexer, col_indexer] = value instead
 
-#warnings.filterwarnings("ignore") #if wanna ignore the warning : A value is trying to be set on a copy of a slice from a DataFrame.Try using .loc[row_indexer,col_indexer] = value instead
->>>>>>> parent of ae195fe (commandLine)
-
-
-
+# Define a function to get an access token
 def get_access_token():
     url = "https://api.baubuddy.de/index.php/login"
     payload = {
@@ -31,83 +26,17 @@ def get_access_token():
     access_token = data.get("oauth", {}).get("access_token")
     return access_token
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-=======
->>>>>>> parent of ae195fe (commandLine)
-
-
+# Get and print the access token
 print(get_access_token())
 
-
-
-<<<<<<< HEAD
->>>>>>> parent of ae195fe (commandLine)
-=======
->>>>>>> parent of ae195fe (commandLine)
+# Define a function for uploading vehicles with CSRF protection disabled
 @csrf_exempt
 def upload_vehicles(request):
     if request.method == 'POST':
         uploaded_file = request.POST.get('csv_data', '')
-
         csv_data = uploaded_file
 
-<<<<<<< HEAD
-        if not csv_data:
-            return JsonResponse({'error': 'Missing CSV file.'}, status=400)
-
-        df = pd.read_csv(StringIO(csv_data), sep=';', header=0)
-        df.dropna(axis=1, how='all', inplace=True)
-
-        access_token = get_access_token()
-        if not access_token:
-            return JsonResponse({'error': 'Failed to retrieve access token.'}, status=500)
-
-        url = "https://api.baubuddy.de/dev/index.php/v1/vehicles/select/active"
-        headers = {
-            "Authorization": f"Bearer {access_token}"
-        }
-
-        response = requests.get(url, headers=headers)
-        if response.status_code != 200:
-            return JsonResponse({'error': f'API request failed with status code {response.status_code}'}, status=500)
-
-        json_data = response.json()
-        json_data_df = pd.DataFrame(json_data)
-
-        merged_df = pd.merge(df, json_data_df, on='kurzname', how='outer')
-
-        merged_df.fillna(method='ffill', inplace=True)
-        merged_df.fillna('', inplace=True)
-
-        hu_filtered_df = merged_df[merged_df['hu'].notna()]
-        hu_filtered_df.at[0, 'labelIds'] = 76
-
-        for index, row in hu_filtered_df.iterrows():
-            label_id = row['labelIds']
-            if label_id:
-                url = f'https://api.baubuddy.de/dev/index.php/v1/labels/{label_id}'
-                headers = {
-                    "Authorization": f"Bearer {access_token}"
-                }
-                response = requests.get(url, headers=headers)
-
-                if response.status_code == 200:
-                    label_data = response.json()
-                    color_code = str(label_data[0]['colorCode'])[1:]
-                    hu_filtered_df.at[index, 'colorCode'] = color_code
-                else:
-                    print(f'Error: There is no true formatted colorCode: {label_id}')
-
-        hu_filtered_df['colorCode'].fillna('', inplace=True)
-        hu_filtered_df['profilePictureUrl'].fillna('', inplace=True)
-        hu_filtered_df['thumbPathUrl'].fillna('', inplace=True)
-
-        hu_filtered_dict = hu_filtered_df.to_dict(orient='records')
-
-        return JsonResponse({'data': hu_filtered_dict}, safe=False)
-=======
+        # Process CSV data
         df = pd.read_csv(StringIO(csv_data), sep=';', header=None)
         header_row = df.iloc[0]
         df = df[1:]
@@ -115,42 +44,37 @@ def upload_vehicles(request):
         df = df.dropna(axis=1, how='all')
         csv_data = df
 
-
         if not csv_data.empty:
+            # Prepare headers and data for making an API request
             url = "https://api.baubuddy.de/dev/index.php/v1/vehicles/select/active"
             headers = {
                 "Authorization": f"Bearer {get_access_token()}"
             }
             response = requests.get(url, headers=headers)
             if response.status_code == 200:
-
+                # Process JSON data from the API
                 json_data = response.json()
-
                 json_data_df = pd.DataFrame(json_data)
-
                 merged_df = csv_data.merge(json_data_df, on='kurzname', how='outer')
 
+                # Format the data
                 merged_df['labelIds_x'] = merged_df['labelIds_x'].astype(str)
                 merged_df['labelIds_y'] = merged_df['labelIds_y'].astype(str)
-
                 merged_df['gruppe_x'].fillna(merged_df['gruppe_y'], inplace=True)
                 merged_df['langtext_x'].fillna(merged_df['langtext_y'], inplace=True)
                 merged_df['info_x'].fillna(merged_df['info_y'], inplace=True)
                 merged_df['lagerort_x'].fillna(merged_df['lagerort_y'], inplace=True)
                 merged_df['labelIds_x'].fillna(merged_df['labelIds_y'], inplace=True)
-
                 merged_df = merged_df.drop(['gruppe_y', 'langtext_y', 'info_y', 'lagerort_y', 'labelIds_y'], axis=1)
                 merged_df.columns = merged_df.columns.str.rstrip('_x')
-
                 hu_filtered_df = merged_df[merged_df['hu'].notna()]
 
-                hu_filtered_df.at[0, 'labelIds'] = 76
+                # Retrieve color codes
+                hu_filtered_df.at[0, 'labelIds'] = 76  # For test
                 hu_filtered_df['colorCode'] = None
                 for column in hu_filtered_df.columns:
                     hu_filtered_df.loc[hu_filtered_df[column].isin(["nan", "", "NaN"]), column] = None
-
                 for index, row in hu_filtered_df.iterrows():
-
                     label_id = row['labelIds']
                     if label_id is not None:
                         url = f'https://api.baubuddy.de/dev/index.php/v1/labels/{label_id}'
@@ -158,36 +82,27 @@ def upload_vehicles(request):
                             "Authorization": f"Bearer {get_access_token()}"
                         }
                         response = requests.get(url, headers=headers)
-
                         if response.status_code == 200:
-
                             label_data = response.json()
                             color_code = label_data[0]['colorCode']
                             color_code = str(color_code[1:])
                             hu_filtered_df.at[index, 'colorCode'] = color_code
-
                         else:
                             print(f'Error: There is no true formatted colorCode: {label_id}')
 
+                # Handle NaN values
                 hu_filtered_df['colorCode'] = hu_filtered_df['colorCode'].apply(lambda x: None if pd.isna(x) else x)
                 hu_filtered_df['profilePictureUrl'] = hu_filtered_df['profilePictureUrl'].apply(
                     lambda x: None if pd.isna(x) else x)
                 hu_filtered_df['thumbPathUrl'] = hu_filtered_df['thumbPathUrl'].apply(
                     lambda x: None if pd.isna(x) else x)
-
                 hu_filtered_df = hu_filtered_df.to_dict(orient='records')
-
             else:
                 print("ERROR:", response.status_code)
 
             response = json.dumps(hu_filtered_df)
-
             return JsonResponse({'data': response}, safe=False)
         else:
-            return JsonResponse({'error': 'CSV verisi eksik.'}, status=400)
-<<<<<<< HEAD
->>>>>>> parent of ae195fe (commandLine)
-=======
->>>>>>> parent of ae195fe (commandLine)
+            return JsonResponse({'error': 'Missing CSV file.'}, status=400)
     else:
-        return JsonResponse({'error': 'Geçersiz istek methodu.'}, status=405)
+        return JsonResponse({'error': 'Invalid request method.'}, status=405)
